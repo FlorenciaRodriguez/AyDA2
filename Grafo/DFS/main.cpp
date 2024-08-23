70115f4379abf8d562bf14ab2d4c9044f28816c1
Florencia <rodrig.florencia@gmail.com>
2024-08-23 19:16:50 -0300

    Update and rename main.cpp to Grafo/DFS/main.cpp

--- Grafo/DFS/main.cpp
+++ Grafo/DFS/main.cpp
@@ -10,18 +10,19 @@ int main(int argc, char **argv)
 	g->agregarVertice('b');
 	g->agregarVertice('c');
 	g->agregarVertice('d');
-    g->agregarVertice('e');
-    g->agregarVertice('f');
-    g->agregarVertice('h');
-    g->agregarVertice('j');
+    	g->agregarVertice('e');
+    	g->agregarVertice('f');
+    	g->agregarVertice('h');
+    	g->agregarVertice('j');
+	
 	g->agregarArco('a', 'b', 1);
-    g->agregarArco('b', 'c', 2);
+    	g->agregarArco('b', 'c', 2);
 	g->agregarArco('c', 'e', 1);
-    g->agregarArco('d', 'c', 2);
+    	g->agregarArco('d', 'c', 2);
 	g->agregarArco('d', 'a', 2);
-    g->agregarArco('e', 'd', 3);
-    g->agregarArco('c', 'f', 2);
-    g->agregarArco('h', 'j', 2);
+    	g->agregarArco('e', 'd', 3);
+    	g->agregarArco('c', 'f', 2);
+    	g->agregarArco('h', 'j', 2);
 
 	dfsForest<char,int>(g);
 	
